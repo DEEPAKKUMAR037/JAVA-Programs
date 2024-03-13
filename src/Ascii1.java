a9637e1a5a4551de8083df86ff9ada855651c4b4
DEEPAKKUMAR037 <deepak78189677@gmail.com>
2024-03-13 09:00:15 +0530

    Ascii1

---
+++ src/Ascii1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ascii1 {
+    public static void main(String[] args) {
+        System.out.print("Enter character :");
+        Scanner ob=new Scanner(System.in);
+        char a=ob.next().charAt(0);
+        int b=a;
+        System.out.print("ASCII VALUE :"+b);
+    }
+
+}
