25c204a038382647e5820facaa9ee0481c1aa1a8
DEEPAKKUMAR037 <deepak78189677@gmail.com>
2024-03-13 08:53:38 +0530

    Addition

---
+++ src/Addition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Addition {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter first number :");
+        float a=obj.nextFloat();
+        System.out.print("Enter second number :");
+        float b=obj.nextFloat();
+        System.out.print("Sum :"+(a+b));
+    }
+}
